e5f1aab004b6389de4d1706e7ec7e39f8ee63f1b
BookYao <workfile_BookYao@163.com>
2020-08-24 10:27:22 +0800

    1. 添加参数的具体解释

--- sippRegisterDemo/branchc.sh
+++ sippRegisterDemo/branchc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # -i: local ip
-# -m: 多少次并发
+# -m: 处理完多少次呼叫后停止请求,也就是最多发送多少个请求
 # -trace_msg: 产生调试日志
 # -trace_err: 产生错误日志
 # 10.0.0.47:7080: 服务器地址
-# -aa: 自动回复 200Ok
+# -l: 同时保持最大的呼叫量(比如注册数)
+# -r 1000 -rp 2000: 设置发送请求的频率, 这里表示：2000ms 发送 1000个请求, 也可以写成 -r 1000 -rp 2s
+# -aa: Info, update 等消息自动回复 200Ok
 
-sipp -sf branchc.xml -inf user.csv -i 192.168.50.132 -m 1000 -trace_msg 10.0.0.47:7080 -aa -trace_err
+sipp -sf branchc.xml -inf user.csv -i 192.168.50.132 -l 1000 -r 1000 -rp 1000 -m 1000 -trace_msg 10.0.0.47:7080 -aa -trace_err
